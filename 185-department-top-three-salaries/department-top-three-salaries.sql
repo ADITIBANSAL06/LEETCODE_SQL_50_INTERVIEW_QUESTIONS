44589fae5d1fd19005db68eeaa0c04918288a48f
ADITIBANSAL06 <148481339+ADITIBANSAL06@users.noreply.github.com>
2025-09-12 19:16:07 +0530

    Time: 1356 ms (19.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select d.name Department, e.name Employee, e.salary Salary
+from Department as d, Employee as e
+where e.departmentId = d.id and 
+(select count(distinct salary) from Employee
+where salary>e.salary and departmentId = e.departmentId)<3
+order by d.name , e.salary desc;
